c7fbee7b5444770ce90ccb8ab3b8bfb2df970db1
Pablo Reig Sánchez <pabloreigsanchez@gmail.com>
2025-03-30 18:28:27 +0200

    Añadida la clase Dormitorio
    Sólo implementado el constructor, siguiendo el esquema de clases

--- banyo.py
+++ banyo.py
@@ -4,9 +4,9 @@ class Banyo(Habitacion):
 
     def __init__(self, superficie : float, tiene_ducha : bool = False, tiene_banyera : bool = False,
                  tiene_vater : bool = False, tiene_lavabo : bool = True):
-        super().__init__(self, superficie)
+        super().__init__(superficie)
         self.__tiene_ducha = tiene_ducha
         self.__tiene_banyera = tiene_banyera
         self.__tiene_vater = tiene_vater
         self.__tiene_lavabo = tiene_lavabo
-        
+
