2dab09bccc4f01b357350ecb7b6a9087e6ae6242
Sama14b <saah1@alu.ua.es>
2025-04-10 20:11:28 +0200

    documentado habitacion

--- modelos/habitacion.py
+++ modelos/habitacion.py
@@ -1,4 +1,22 @@
 class Habitacion:
+    """
+    Clase que representa una habitación dentro de un inmueble.
+
+    Atributos privados:
+        __superficie (float): Superficie de la habitación en metros cuadrados.
+
+    Métodos:
+        __init__(superficie): Constructor que inicializa la superficie de la habitación.
+
+    Ejemplo de uso:
+        h1 = Habitacion(12.5)
+    """
     def __init__(self, superficie : float):
-            self.__superficie = superficie
+        """
+        Inicializa una nueva instancia de Habitacion.
+
+        Parámetros:
+            superficie (float): Superficie de la habitación en m².
+        """
+        self.__superficie = superficie
 
