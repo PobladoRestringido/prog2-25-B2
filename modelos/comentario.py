8baa8e339c5e39d0830d533b0f74787980a7edde
Adrian-Diez-Agullo <ada50@alu.ua.es>
2025-04-10 17:42:14 +0200

    Documentacion de publicaion

--- modelos/comentario.py
+++ modelos/comentario.py
@@ -1,7 +1,32 @@
-from modelos.comprador import Comprador # lo importamos para poder usarlo en type-hinting
+from modelos.comprador import Comprador  # Lo importamos para poder usarlo en type-hinting
 
 class Comentario:
-    def __init__(self, contenido : str, autor : Comprador, fecha_publicacion : str):
+    """
+    Clase que representa un comentario realizado por un comprador sobre una publicación.
+
+    Attributes
+    ----------
+    contenido : str
+        Texto del comentario realizado.
+    autor : Comprador
+        Objeto que representa al comprador que hizo el comentario.
+    fecha_publicacion : str
+        Fecha en la que se realizó el comentario (como cadena de texto).
+    """
+
+    def __init__(self, contenido: str, autor: Comprador, fecha_publicacion: str):
+        """
+        Inicializa una nueva instancia de la clase Comentario.
+
+        Parameters
+        ----------
+        contenido : str
+            Contenido del comentario.
+        autor : Comprador
+            Comprador que realizó el comentario.
+        fecha_publicacion : str
+            Fecha del comentario (en formato de texto, por ejemplo '2025-01-23').
+        """
         self.__contenido = contenido
         self.__autor = autor
         self.__fecha_publicacion = fecha_publicacion
