214305e4d37f6b42cb0b88d34410a8ab5d77635f
Adrian-Diez-Agullo <ada50@alu.ua.es>
2025-04-10 20:27:23 +0200

    Update zona_geografica.py

--- modelos/zona_geografica.py
+++ modelos/zona_geografica.py
@@ -1,9 +1,38 @@
-from typing import List # esto permite utilizar List en type-hinting
-from modelos.inmueble import Inmueble # esto permite utilizar Inmueble en type-hinting
+from typing import List  # Esto permite utilizar List en type-hinting
+from modelos.inmueble import Inmueble  # Esto permite utilizar Inmueble en type-hinting
 
 class ZonaGeografica:
+    """
+    Clase que representa una zona geográfica donde se encuentran ubicados distintos inmuebles.
 
-    def __init__(self, nombre : str, pais : str, inmuebles : List[Inmueble, ...]) -> None:
+    Atributos
+    ----------
+    nombre : str
+        Nombre de la zona geográfica.
+
+    pais : str
+        País al que pertenece la zona geográfica.
+
+    inmuebles : List[Inmueble]
+        Lista de inmuebles ubicados en esta zona.
+    """
+
+    def __init__(self, nombre: str, pais: str, inmuebles: List[Inmueble,...]) -> None:
+        """
+        Inicializa una nueva instancia de la clase ZonaGeografica.
+
+        Parametros
+        ----------
+        nombre : str
+            Nombre de la zona geográfica.
+
+        pais : str
+            País al que pertenece la zona.
+
+        inmuebles : List[Inmueble]
+            Lista de inmuebles que pertenecen a esta zona geográfica.
+        """
         self.__nombre = nombre
         self.__pais = pais
         self.__inmuebles = inmuebles
+
