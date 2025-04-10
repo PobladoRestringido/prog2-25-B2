fb95cfaf9a9bb48eee03f35f77db5d50a3802fcb
david <dmt28@alu.ua.es>
2025-04-10 11:00:49 +0200

    terminada la clase informe_PDF

--- informe_pdf.py
+++ informe_pdf.py
@@ -3,13 +3,38 @@ from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
 
 class InformePDF():
+    """
+    Una clase usaada para crear un objeto informe que luego se puede usar para generar un informe PDF a partir de una lista de inmuebles
 
-    def __init__(self, nombre_archivo):
+    Atributos
+    ---------
+    nombre_archivo : str
+        el nombre del archivo
+
+    Métodos
+    -------
+    generar_informe(inmuebles : list)
+        Genera un informe PDF en base a una lista de inmuebles proporcionada
+    """
+    def __init__(self, nombre_archivo : str = 'informe'):
+        """
+        Parámetros
+        nombre_archivo : str
+            El nombre del archivo
+        """
         self.nombre_archivo = nombre_archivo
 
-    def generar_informe(self, inmuebles):
+    def generar_informe(self, inmuebles : list):
+        """
+        Genera el informe con la lista de inmuebles proporcionada
+
+        Parámetros
+        ----------
+        inmuebles : list
+            Lista de objetos de tipo Inmueble
+        """
         c = canvas.Canvas(self.nombre_archivo, pagesize=A4)
-        x.setTitle("Informe de Inmuebles")
+        c.setTitle("Informe de Inmuebles")
 
         # Encabezado
         c.setFont("Helvetica-Bold", 18)
@@ -23,14 +48,14 @@ class InformePDF():
         y_pos = 770
 
         for idx, inmueble in enumerate(inmuebles, start=1):
-            c.drawString(100, y_pos, f"{idx}.{inmueble.nombre}")
+            c.drawString(100, y_pos, f"{idx}.{inmueble}")
             y_pos -= 15
 
-            # Salto de página si no cabe más textp
+            # Salto de página si no cabe más texto
             if y_pos < 50:
-                c.showPage() # crear pagina
-                y_pos = 800 # reiniciar posicion y
-                c.setFont("Helvetica", 12) # restaurar fuente
+                c.showPage()
+                y_pos = 800
+                c.setFont("Helvetica", 12)
 
         # guardar pdf
         c.save()
