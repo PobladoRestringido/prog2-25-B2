2632c2636a444cbba7c4538a2d3cda38adc71a03
Luciamcs <lmc146@alu.ua.es>
2025-05-21 21:15:34 +0200

    API auxiliar para vincularlo con las clases
    Api por si no se puede la base de datos

---
+++ API1.py
@@ -0,0 +1,44 @@
+from comentarios_inmu import comentarios_casas, comentarios_pisos,comentarios_usuario
+from modelos.usuario.comprador import Comprador
+from modelos.usuario.administrador import Administrador
+from modelos.usuario.vendedor import Vendedor
+from modelos.inmueble.piso import Piso
+from modelos.inmueble.vivienda_unifamiliar import ViviendaUnifamiliar
+from modelos.habitacion.dormitorio import Dormitorio
+from modelos.habitacion.cocina import Cocina
+from modelos.habitacion.banyo import Banyo
+from modelos.habitacion.salon import Salon
+
+from examples.inmuebles_ejemplo import inmuebles
+from examples.vendedor_ejemplo import vendedores
+from examples.Zonas_ejemplo import zonas
+
+from serializacion.pickling import cargar_data
+
+import random
+from datetime import datetime
+# para guardar la fecha de creación de Publicaciones y Comentarios.
+
+from flask import Flask, jsonify, request, Response
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, get_jwt
+import sqlite3
+from openai import OpenAI
+import time
+
+app = Flask(__name__) #Creamos la aplicación Flask
+app.config['JWT_SECRET_KEY'] = 'clave_super_secreta'  #Clave para autentificar
+jwt = JWTManager(app)
+usuarios_registrados = []
+
+@app.route('/') #Ruta inicial de la api
+def casa():
+    """
+       Función de inicio de la API. Esta función maneja la ruta raíz y
+       devuelve un mensaje de bienvenida.
+
+       Devuélve
+       --------------
+        -str: Un mensaje de texto dando la bienvenida a la API.
+    """
+    return 'Bienvenido a la API de inmuebles'
+
