6f25b6d7f9da8c7b2051914b5d10656d759e3f99
Luciamcs <lmc146@alu.ua.es>
2025-05-21 12:21:58 +0200

    Cambios en api
    He cambiado algunas cosas de la API y he añadido comentarios de ejemplos

---
+++ examples/comprador_ejemplo.py
@@ -0,0 +1,7 @@
+from modelos.usuario.comprador import Comprador
+
+comprador1 = Comprador("ana.gomez", "claveAna123")
+comprador2 = Comprador("juan.perez", "claveJuan456")
+comprador3 = Comprador("laura.martin", "claveLaura789")
+
+compradores=[comprador1,comprador2,comprador3]
