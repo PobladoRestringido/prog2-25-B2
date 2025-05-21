8fc0b314d38adac313fb94e3e3716ddc981711aa
Sama14b <saah1@alu.ua.es>
2025-05-21 21:38:21 +0200

    ultimo arreglo de casas y pisos

--- examples/inmuebles_ejemplo.py
+++ examples/inmuebles_ejemplo.py
@@ -10,7 +10,7 @@ from examples.Zonas_ejemplo import zonas
 from examples.vendedor_ejemplo import vendedores
 
 # Crear habitaciones para piso
-habitaciones_piso = [
+habitaciones_piso1 = [
     Dormitorio(12.0, tiene_cama=True, tiene_lampara=True),
     Cocina(8.0, tiene_frigorifico=True, tiene_horno=True),
     Banyo(5.0, tiene_ducha=True, tiene_lavabo=True),
@@ -29,8 +29,21 @@ habitaciones_piso3 = [
     Salon(16.0, tiene_sofa=True)
 ]
 
+habitaciones_piso4 = [
+    Dormitorio(13.0, tiene_cama=True),
+    Cocina(9.0, tiene_frigorifico=True, tiene_horno=True, tiene_microondas=True),
+    Banyo(5.5, tiene_ducha=True, tiene_lavabo=True),
+    Salon(21.0, tiene_televisor=True, tiene_sofa=True)
+]
+habitaciones_piso5 = [
+    Dormitorio(14.0, tiene_cama=True, tiene_mesa_estudio=True),
+    Cocina(7.0, tiene_frigorifico=True),
+    Banyo(4.0, tiene_ducha=True, tiene_lavabo=True),
+    Salon(19.0, tiene_sofa=True, tiene_televisor=True)
+]
+
 # Crear habitaciones para vivienda unifamiliar
-habitaciones_casa = [
+habitaciones_casa1 = [
     Dormitorio(15.0, tiene_cama=True),
     Cocina(10.0, tiene_frigorifico=True),
     Banyo(7.0, tiene_banyera=True, tiene_lavabo=True),
@@ -51,11 +64,25 @@ habitaciones_casa3 = [
     Banyo(5.0, tiene_ducha=True),
     Salon(20.0, tiene_sofa=True, tiene_televisor=True)
 ]
+habitaciones_casa4 = [
+    Dormitorio(13.0, tiene_cama=True),
+    Dormitorio(10.0, tiene_mesa_estudio=True),
+    Cocina(8.0, tiene_frigorifico=True, tiene_microondas=True),
+    Banyo(6.0, tiene_ducha=True, tiene_lavabo=True),
+    Salon(23.0, tiene_sofa=True, tiene_televisor=True)
+]
+habitaciones_casa5 = [
+    Dormitorio(17.0, tiene_cama=True, tiene_lampara=True),
+    Dormitorio(14.0, tiene_cama=True),
+    Cocina(11.0, tiene_frigorifico=True, tiene_horno=True),
+    Banyo(7.0, tiene_banyera=True, tiene_lavabo=True),
+    Salon(26.0, tiene_sofa=True, tiene_televisor=True)
+]
 
 piso1 = Piso(
     nombre="Piso Luminoso",
     descripcion="Piso céntrico con buenas vistas",
-    habitaciones=habitaciones_piso,
+    habitaciones=habitaciones_piso1,
     precio=180000,
     zona=zonas["centro_madrid"],
     duenyo=vendedores[0],
@@ -81,6 +108,27 @@ piso3 = Piso(
     duenyo=vendedores[4],
     planta=2,
     ascensor=True
+)
+piso4 = Piso(
+    nombre="Piso Equipado",
+    descripcion="Perfecto para parejas jóvenes",
+    habitaciones=habitaciones_piso4,
+    precio=195000,
+    zona=zonas["este_madrid"],
+    duenyo=vendedores[1],
+    planta=4,
+    ascensor=True
+)
+
+piso5 = Piso(
+    nombre="Piso Económico",
+    descripcion="Buena opción para inversión",
+    habitaciones=habitaciones_piso5,
+    precio=145000,
+    zona=zonas["sur_madrid"],
+    duenyo=vendedores[3],
+    planta=1,
+    ascensor=False
 )
 
 
@@ -114,19 +162,47 @@ casa3 = ViviendaUnifamiliar(
     tiene_piscina=True,
     jardin="Gran patio con árboles frutales"
 )
+casa4 = ViviendaUnifamiliar(
+    duenyo=vendedores[2],
+    descripcion="Casa moderna con terraza y garaje",
+    precio=410000,
+    nombre="Casa Moderna",
+    habitaciones=habitaciones_casa4,
+    zona=zonas["centro_madrid"],
+    tiene_piscina=False,
+    jardin="Terraza amplia con vistas"
+)
+
+casa5 = ViviendaUnifamiliar(
+    duenyo=vendedores[4],
+    descripcion="Casa rústica con encanto natural",
+    precio=370000,
+    nombre="Casa Rústica",
+    habitaciones=habitaciones_casa5,
+    zona=zonas["sur_madrid"],
+    tiene_piscina=True,
+    jardin="Jardín con vegetación autóctona"
+)
+
 
 # Añadir inmuebles a las zonas correspondientes
 zonas["centro_madrid"].agregar_inmueble(piso1)
+zonas["centro_madrid"].agregar_inmueble(casa5)
+
 zonas["norte_madrid"].agregar_inmueble(casa1)
+zonas["norte_madrid"].agregar_inmueble(casa3)
 
 zonas["sur_madrid"].agregar_inmueble(piso2)
+zonas["sur_madrid"].agregar_inmueble(piso5)
+zonas["sur_madrid"].agregar_inmueble(casa4)
+
+zonas["este_madrid"].agregar_inmueble(piso4)
 zonas["este_madrid"].agregar_inmueble(casa2)
 
 zonas["oeste_madrid"].agregar_inmueble(piso3)
-zonas["norte_madrid"].agregar_inmueble(casa3)
 
 # Lista de inmuebles para trabajar
-inmuebles = [piso1, casa1, piso2, casa2, piso3, casa3]
+inmuebles = [piso1, casa1, piso2, casa2, piso3, casa3, piso4, casa4, piso5, casa5]
 
 for i, inmueble in enumerate(inmuebles, 1):
     print(f"Inmueble {i}: {inmueble.nombre} - Precio: {inmueble.precio} € - Zona: {inmueble.zona.nombre}")
